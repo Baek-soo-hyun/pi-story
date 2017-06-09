16d84e8092d47ece7ea88e01f1c71dd94f1aa5c1
Baek-soo-hyun <100suhyun@naver.com>
2017-06-09 13:06:57 +0900

    Initial commit

---
+++ src/js/common.js
@@ -0,0 +1,18 @@
+define([
+	"bootstrap",
+], function() {
+	// Home btn 클릭 시
+	$(".site-name>img").on("click", function() {
+		$(".site-name>img.default").css("display", "none");
+		$(".site-name>img.active").css("display", "block");
+
+		location.href = window._ctx.root;
+	});
+
+	// 카테고리 클릭 시
+	$(".category>ul>li").on("click", function() {
+		var category = $(this).attr("value");
+		$(".category>ul>li").removeClass("active");
+		$(".category>ul>li[value=" + category + "]").addClass("active");
+	});
+});
